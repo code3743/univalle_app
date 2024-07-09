edb52e98aed30fe9a0654601b34ba3b3cca6bdb6
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-09 14:29:15 -0500

    Initial project setup

---
+++ lib/config/routers/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:univalle_app/features/auth/presentation/screens/login_screen.dart';
+
+final appRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(routes: [
+    GoRoute(
+      path: '/',
+      name: 'login',
+      builder: (context, state) => const LoginScreen(),
+    )
+  ]);
+});
