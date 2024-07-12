46abf2933a41d5bd7d5ace21190af164027e29ff
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-12 17:18:12 -0500

    feat: Add SiraException class for custom exceptions

---
+++ lib/core/errors/sira_exception.dart
@@ -0,0 +1,11 @@
+class SiraException implements Exception {
+  final String message;
+  final String? code;
+
+  SiraException(this.message, {this.code});
+
+  @override
+  String toString() {
+    return 'SiraExecption: $message';
+  }
+}
