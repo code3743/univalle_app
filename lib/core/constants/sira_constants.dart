1194dbfbbdc8b8ab9ce130cf88ea586de29146fa
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-12 17:17:57 -0500

    feat: Add SiraConstants class with base URLs and paths

---
+++ lib/core/constants/sira_constants.dart
@@ -0,0 +1,8 @@
+class SiraConstants {
+  static const String baseUrl = 'https://sira.univalle.edu.co/sra/';
+  static const String logoutPath = '/paquetes/autenticacion/salir.php';
+  static const String homePath =
+      '/paquetes/inicioestudiante/index.php?accion=Inicio';
+  static const String studentPath = '/paquetes/tablaMaestro/persona/index.php';
+  static const String rantingPath = '/paquetes/academica/index.php';
+}
