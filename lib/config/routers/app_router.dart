20739a1a64957a17d539c1daeb74f69fb89f0c85
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-15 15:00:54 -0500

    feat: Add home screen route and corresponding screen

--- lib/config/routers/app_router.dart
+++ lib/config/routers/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:univalle_app/features/auth/presentation/screens/login_screen.dart';
+import 'package:univalle_app/features/home/presentation/screens/home_screen.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(routes: [
+  return GoRouter(initialLocation: '/home', routes: [
     GoRoute(
-      path: '/',
+      path: '/login',
       name: 'login',
       builder: (context, state) => const LoginScreen(),
-    )
+    ),
+    GoRoute(
+      path: '/home',
+      name: 'home',
+      builder: (context, state) => const HomeScreen(),
+    ),
   ]);
 });
