019499c89ec69e082a3c6f4135adee3c37aafb16
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-16 15:37:00 -0500

    feat: Update dependencies for barcode and shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:univalle_app/config/themes/app_theme.dart';
 import 'package:univalle_app/config/routers/app_router.dart';
+import 'package:univalle_app/core/providers/shared_preferences_provider.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  return runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
