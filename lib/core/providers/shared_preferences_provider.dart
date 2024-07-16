cb4d32724d750e859e0948a6be4b851bf71a6b81
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-16 15:36:06 -0500

    feat: Add SharedStudentUtility provider for SharedPreferences

---
+++ lib/core/providers/shared_preferences_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferencesProvider =
+    Provider<SharedPreferences>((ref) => throw UnimplementedError());
+
+final sharedUtilityProvider = Provider<SharedStudentUtility>((ref) {
+  final sharedPrefs = ref.watch(sharedPreferencesProvider);
+  return SharedStudentUtility(sharedPrefs);
+});
+
+class SharedStudentUtility {
+  final SharedPreferences _sharedPreferences;
+  SharedStudentUtility(this._sharedPreferences);
+
+  String? getToken() {
+    return _sharedPreferences.getString('token');
+  }
+
+  String? getStudentId() {
+    return _sharedPreferences.getString('studentId');
+  }
+
+  String? getPassword() {
+    return _sharedPreferences.getString('password');
+  }
+
+  Future<void> saveStudentData(
+      String token, String studentId, String password) async {
+    await _sharedPreferences.setString('token', token);
+    await _sharedPreferences.setString('studentId', studentId);
+    await _sharedPreferences.setString('password', password);
+  }
+}
