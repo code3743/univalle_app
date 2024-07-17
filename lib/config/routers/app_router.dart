4a3def95f0a00f4647374619289fdbd257e4f87a
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-17 16:49:55 -0500

    feat: Add TabulateScreen widget for tabulating student data

--- lib/config/routers/app_router.dart
+++ lib/config/routers/app_router.dart
@@ -3,9 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:univalle_app/features/auth/presentation/screens/login_screen.dart';
 import 'package:univalle_app/features/home/presentation/screens/home_screen.dart';
 import 'package:univalle_app/features/student_grades/presentation/screens/student_grades_screen.dart';
+import 'package:univalle_app/features/student_tabulate/presentation/screens/tabulate_screen.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(initialLocation: '/student-grades', routes: [
+  return GoRouter(initialLocation: '/tabulate', routes: [
     GoRoute(
       path: '/login',
       name: 'login',
@@ -21,5 +22,9 @@ final appRouterProvider = Provider<GoRouter>((ref) {
       name: 'student-grades',
       builder: (context, state) => const StudentGradesScreen(),
     ),
+    GoRoute(
+        path: '/tabulate',
+        name: 'tabulate',
+        builder: (context, state) => const TabulateScreen()),
   ]);
 });
