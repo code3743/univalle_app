fe9e71bbc3a26ed7d9fce9552f29606fd9d6af4d
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-19 15:15:07 -0500

    feat: Update app_router.dart to use GlobalKey for navigation

--- lib/config/routers/app_router.dart
+++ lib/config/routers/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart' show GlobalKey, NavigatorState;
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:univalle_app/features/home/presentation/screens/home_screen.dart';
@@ -8,41 +9,47 @@ import 'package:univalle_app/features/program_resolution/presentation/screens/re
 import 'package:univalle_app/features/student_grades/presentation/screens/student_grades_screen.dart';
 import 'package:univalle_app/features/program_resolution/presentation/screens/detail_semester_screen.dart';
 
+final GlobalKey<NavigatorState> _globalKey = GlobalKey<NavigatorState>();
+
 final appRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(initialLocation: '/home/resolution', routes: [
-    GoRoute(
-      path: '/login',
-      name: 'login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-        path: '/home',
-        name: 'home',
-        builder: (context, state) => const HomeScreen(),
-        routes: [
-          GoRoute(
-            path: 'student-grades',
-            name: 'student-grades',
-            builder: (context, state) => const StudentGradesScreen(),
-          ),
-          GoRoute(
-              path: 'tabulate',
-              name: 'tabulate',
-              builder: (context, state) => const TabulateScreen()),
-          GoRoute(
-              path: 'resolution',
-              name: 'resolution',
-              builder: (context, state) => const ResolutionScreen(),
-              routes: [
-                GoRoute(
-                    path: 'detail-semester',
-                    name: 'detail-semester',
-                    builder: (context, state) {
-                      final SubjectCycle subjectCycle =
-                          state.extra as SubjectCycle;
-                      return DetailSemesterScreen(subjectCycle: subjectCycle);
-                    })
-              ]),
-        ]),
-  ]);
+  return GoRouter(
+    initialLocation: '/home/resolution',
+    navigatorKey: _globalKey,
+    routes: [
+      GoRoute(
+        path: '/login',
+        name: 'login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+          path: '/home',
+          name: 'home',
+          builder: (context, state) => const HomeScreen(),
+          routes: [
+            GoRoute(
+              path: 'student-grades',
+              name: 'student-grades',
+              builder: (context, state) => const StudentGradesScreen(),
+            ),
+            GoRoute(
+                path: 'tabulate',
+                name: 'tabulate',
+                builder: (context, state) => const TabulateScreen()),
+            GoRoute(
+                path: 'resolution',
+                name: 'resolution',
+                builder: (context, state) => const ResolutionScreen(),
+                routes: [
+                  GoRoute(
+                      path: 'detail-semester',
+                      name: 'detail-semester',
+                      builder: (context, state) {
+                        final SubjectCycle subjectCycle =
+                            state.extra as SubjectCycle;
+                        return DetailSemesterScreen(subjectCycle: subjectCycle);
+                      })
+                ]),
+          ]),
+    ],
+  );
 });
