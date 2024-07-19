938a22154a6476a8c73da4a6fc4ddca2c78835bf
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-19 11:18:11 -0500

    feat: Add CourseEvaluation constants for course evaluation URLs

---
+++ lib/core/constants/course_evaluation.dart
@@ -0,0 +1,5 @@
+class CourseEvaluation {
+  static const String baseUrl =
+      'https://evaluacioncursos.univalle.edu.co/evaluaciondocente/';
+  static const String homePath = '/paquetes/modulos/index.php?ID=ID';
+}
