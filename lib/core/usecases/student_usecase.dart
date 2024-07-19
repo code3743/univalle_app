eba0c9656148ffe99fca28dcf18e41c31c590a29
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-19 15:15:39 -0500

    feat: Add StudentUseCase class for managing student-related functionality

---
+++ lib/core/usecases/student_usecase.dart
@@ -0,0 +1,54 @@
+import 'package:univalle_app/features/auth/domain/entities/student.dart';
+import 'package:univalle_app/features/student_grades/domain/entities/grades.dart';
+import 'package:univalle_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:univalle_app/features/student_tabulate/domain/entities/tabulate.dart';
+import 'package:univalle_app/features/program_resolution/domain/entities/subject_cycle.dart';
+import 'package:univalle_app/features/student_grades/domain/repositories/grades_repository.dart';
+import 'package:univalle_app/features/student_tabulate/domain/repositories/tabulate_repository.dart';
+import 'package:univalle_app/features/program_resolution/domain/repositories/resolution_repository.dart';
+
+class StudentUseCase {
+  final AuthRepository _authRepository;
+
+  final GradesRepository _gradesRepository;
+
+  final TabulateRepository _tabulateRepository;
+
+  final ResolutionRepository _resolutionRepository;
+  late Student _student;
+
+  StudentUseCase(
+    this._authRepository,
+    this._gradesRepository,
+    this._tabulateRepository,
+    this._resolutionRepository,
+  );
+
+  Future<void> login(String code, String password) async {
+    await _authRepository.login(code, password);
+    _student = await _authRepository.getStudent();
+  }
+
+  Future<void> logout() async {
+    await _authRepository.logout();
+  }
+
+  Student getStudent() {
+    return _student;
+  }
+
+  Future<List<Grades>> getGrades() async {
+    return await _gradesRepository.getGrades(
+        _student.token, _student.studentId.substring(2));
+  }
+
+  Future<Tabulate> getTabulate() async {
+    return await _tabulateRepository.getTabulate(
+        _student.studentId.substring(2), _student.programId, _student.token);
+  }
+
+  Future<List<SubjectCycle>> getResolution() async {
+    return await _resolutionRepository.getResolution(
+        _student.token, _student.studentId, _student.programId);
+  }
+}
