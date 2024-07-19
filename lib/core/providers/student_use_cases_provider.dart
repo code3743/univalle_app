ec4e0a7e3d07689c5c608b4199b4d1f3ce667ded
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-19 15:16:09 -0500

    feat: Add StudentUseCasesProvider for managing student-related functionality

---
+++ lib/core/providers/student_use_cases_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:univalle_app/core/providers/shared_preferences_provider.dart';
+import 'package:univalle_app/core/usecases/student_usecase.dart';
+import 'package:univalle_app/features/auth/data/datasource/sira_auth_datasource.dart';
+import 'package:univalle_app/features/auth/data/repositories/sira_auth_repository_impl.dart';
+import 'package:univalle_app/features/program_resolution/data/datasource/sira_resolution_datasource.dart';
+import 'package:univalle_app/features/program_resolution/data/repositories/sira_resolution_repository_impl.dart';
+import 'package:univalle_app/features/student_grades/data/datasources/sira_grades_datasource.dart';
+import 'package:univalle_app/features/student_grades/data/repositories/sira_grades_repository_impl.dart';
+import 'package:univalle_app/features/student_tabulate/data/datasource/sira_tabulate_datasource.dart';
+import 'package:univalle_app/features/student_tabulate/data/repositories/sira_tabulate_repository_impl.dart';
+
+final studentUseCasesProvider = Provider<StudentUseCase>((ref) {
+  final sharedStudentUtility = ref.watch(sharedUtilityProvider);
+  final authRepository =
+      SiraAuthRepositoryImpl(SiraAuthDatasource(sharedStudentUtility));
+  final studentGradesRepository =
+      SiraGradesRepositoryImpl(SiraGradesDatasource());
+  final studentTabulateRepository =
+      SiraTabulateRepositoryImpl(SiraTabulateDatasource());
+  final resolutionRepository =
+      SiraResolutionRepositoryImpl(SiraResolutionDatasource());
+
+  return StudentUseCase(authRepository, studentGradesRepository,
+      studentTabulateRepository, resolutionRepository);
+});
