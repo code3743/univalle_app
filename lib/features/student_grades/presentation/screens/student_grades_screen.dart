383584745c127c9c0a08ee9bceec30a9f26c50dd
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-21 11:21:51 -0500

    feat: Update student grades screen with Riverpod integration and loading indicator

--- lib/features/student_grades/presentation/screens/student_grades_screen.dart
+++ lib/features/student_grades/presentation/screens/student_grades_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:univalle_app/config/themes/app_colors.dart';
 import 'package:univalle_app/features/student_grades/domain/entities/grades.dart';
 import 'package:univalle_app/features/student_grades/domain/entities/subject.dart';
+import 'package:univalle_app/features/student_grades/presentation/providers/student_grades_provider.dart';
 import 'package:univalle_app/features/student_grades/presentation/widgets/grades_overview.dart';
 import 'package:univalle_app/features/student_grades/presentation/widgets/period_grades_list.dart';
 
-class StudentGradesScreen extends StatelessWidget {
+class StudentGradesScreen extends ConsumerWidget {
   const StudentGradesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final studentGrade = ref.watch(studentGradesProvider);
+    final studentGradeNotifier = ref.read(studentGradesProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calificaciones'),
@@ -17,79 +22,58 @@ class StudentGradesScreen extends StatelessWidget {
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-          children: [
-            const PeriodGradesList(periods: ['Todos', '2021-1']),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 8),
-                      child: GradesOverview(
-                        grade: Grades(
-                            period: '2021-1',
-                            average: 3.5,
-                            credits: 20,
-                            porcentageApproved: '80%',
-                            subjects: [
-                              Subject(
-                                  name: 'Matematicas',
-                                  code: 'MAT-101',
-                                  group: 'A',
-                                  credits: 4,
-                                  grade: 4.5,
-                                  isCanceled: false,
-                                  isEnabled: false),
-                              Subject(
-                                  name: 'Fisica',
-                                  code: 'FIS-101',
-                                  group: 'A',
-                                  credits: 4,
-                                  grade: 3.5,
-                                  isCanceled: false,
-                                  isEnabled: false),
-                              Subject(
-                                  name: 'Quimica',
-                                  code: 'QUI-101',
-                                  group: 'A',
-                                  credits: 4,
-                                  grade: 2.9,
-                                  isCanceled: false,
-                                  isEnabled: false),
-                              Subject(
-                                  name: 'Programacion',
-                                  code: 'PRO-101',
-                                  group: 'A',
-                                  credits: 4,
-                                  grade: 4.0,
-                                  isCanceled: false,
-                                  isEnabled: false),
-                              Subject(
-                                  name: 'Base de Datos',
-                                  code: 'BD-101',
-                                  group: 'A',
-                                  credits: 4,
-                                  grade: 3.5,
-                                  isCanceled: false,
-                                  isEnabled: false),
-                              Subject(
-                                  name: 'Ingles',
-                                  code: 'ING-101',
-                                  group: 'A',
-                                  credits: 4,
-                                  grade: 0,
-                                  isCanceled: true,
-                                  isEnabled: false),
-                            ],
-                            isWorthy: false),
-                      ));
-                },
-                itemCount: 1,
+        child: studentGrade == null
+            ? const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator.adaptive(
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(AppColors.primaryRed)),
+                  SizedBox(height: 10),
+                  Text(
+                    'Consultando calificaciones...',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.primaryRed),
+                  )
+                ],
+              )
+            : Column(
+                children: [
+                  PeriodGradesList(periods: studentGradeNotifier.periods),
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        final grades = studentGrade[index];
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 8),
+                            child: GradesOverview(
+                              grade: Grades(
+                                  period: grades.period,
+                                  average: grades.average,
+                                  credits: grades.credits,
+                                  porcentageApproved: grades.porcentageApproved,
+                                  subjects: [
+                                    for (var subject in grades.subjects)
+                                      Subject(
+                                          name: subject.name,
+                                          code: subject.code,
+                                          group: subject.group,
+                                          credits: subject.credits,
+                                          grade: subject.grade,
+                                          isCanceled: subject.isCanceled,
+                                          isEnabled: subject.isEnabled)
+                                  ],
+                                  isWorthy: grades.isWorthy),
+                            ));
+                      },
+                      itemCount: studentGrade.length,
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
