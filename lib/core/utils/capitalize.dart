2dd5fc6f99edbc3affec5a18599faf8c30132dac
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-22 16:46:14 -0500

    refactor: Improve capitalize extension in String class

--- lib/core/utils/capitalize.dart
+++ lib/core/utils/capitalize.dart
@@ -1,8 +1,8 @@
 extension StringExtension on String {
   String capitalize() {
-    if (length < 3) return this;
-    return split(' ')
-        .map((e) => '${e.toUpperCase()}${e.substring(1).toLowerCase()}')
-        .join(' ');
+    return split(' ').map((e) {
+      if (e.length < 3) return e.toLowerCase();
+      return '${e[0].toUpperCase()}${e.substring(1).toLowerCase()}';
+    }).join(' ');
   }
 }
