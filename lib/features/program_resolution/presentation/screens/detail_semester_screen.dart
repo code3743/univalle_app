1d28823d782f83027b6a3899215ad4c02784ef78
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-23 20:21:54 -0500

    refactor: Update detail semester screen layout and styling

--- lib/features/program_resolution/presentation/screens/detail_semester_screen.dart
+++ lib/features/program_resolution/presentation/screens/detail_semester_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:univalle_app/config/themes/app_colors.dart';
 import 'package:univalle_app/features/program_resolution/domain/entities/subject_cycle.dart';
 
 class DetailSemesterScreen extends StatelessWidget {
@@ -8,11 +9,110 @@ class DetailSemesterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Semestre ${subjectCycle.semester}'),
+          title: const Text('Detalle del semestre'),
         ),
-        body: const SizedBox(
+        body: SizedBox(
             width: double.infinity,
             height: double.infinity,
-            child: CustomScrollView()));
+            child: CustomScrollView(
+              slivers: [
+                const SliverToBoxAdapter(
+                    child: SizedBox(
+                  height: 20,
+                )),
+                SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                  (_, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: AppColors.white,
+                        clipBehavior: Clip.antiAlias,
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading: Column(
+                                children: [
+                                  const Text('Créditos'),
+                                  Text(subjectCycle.subjects[index].credits,
+                                      style: const TextStyle(
+                                          fontSize: 26,
+                                          fontWeight: FontWeight.w500))
+                                ],
+                              ),
+                              textColor: AppColors.grey,
+                              title: Text(subjectCycle.subjects[index].name),
+                              subtitle: Text(subjectCycle.subjects[index].code),
+                            ),
+                            Container(
+                              width: double.infinity,
+                              color: AppColors.primaryBlue,
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                itemBuilder: (_, i) {
+                                  if (i == 0) {
+                                    return const Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Prerrequisitos',
+                                        style: TextStyle(
+                                          color: AppColors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    );
+                                  }
+
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.circle,
+                                          color: AppColors.green,
+                                          size: 16,
+                                        ),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        SizedBox(
+                                          width:
+                                              MediaQuery.sizeOf(context).width *
+                                                  .75,
+                                          child: Text(
+                                            subjectCycle.subjects[index]
+                                                .prerequisites[i - 1],
+                                            style: const TextStyle(
+                                              color: AppColors.white,
+                                              fontSize: 12,
+                                            ),
+                                            maxLines: 5,
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                                itemCount: subjectCycle
+                                    .subjects[index].prerequisites.length,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: subjectCycle.subjects.isNotEmpty
+                      ? subjectCycle.subjects.length + 1
+                      : 0,
+                )),
+                const SliverToBoxAdapter(
+                    child: SizedBox(
+                  height: 20,
+                )),
+              ],
+            )));
   }
 }
