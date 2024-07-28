d7fe56beff857e36a3f7128ed267c96b248fe823
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-27 19:27:50 -0500

    feat: Add TeachingRatingDatasource and TeachingRatingRepository classes

---
+++ lib/features/teaching_rating/domain/repositories/teaching_rating_repository.dart
@@ -0,0 +1,8 @@
+import 'package:univalle_app/features/teaching_rating/domain/entities/teaching_rating.dart';
+
+abstract class TeachingRatingRepository {
+  Future<String> authenticateSession(String user, String password);
+  Future<List<TeachingRating>> getTeachingQualifications(String sessionId);
+  Future<void> sendTeachingQualification(
+      TeachingRating teacher, String sessionId);
+}
