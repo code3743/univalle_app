4d9bd9423e58911ebbcf5d5ac45afeac59ab7955
code3743 <jota.lopez@correounivalle.edu.co>
2024-07-27 19:27:56 -0500

    feat: Add TeachingRatingModel for teaching rating data model

---
+++ lib/features/teaching_rating/data/models/teaching_rating_model.dart
@@ -0,0 +1,103 @@
+import 'package:univalle_app/features/teaching_rating/domain/entities/teaching_rating.dart';
+
+class TeachingRatingModel {
+  final String id;
+  final String teacherName;
+  final String subjectName;
+  final String code;
+  final String group;
+  final String campusId;
+  final String teacherId;
+  final String teacherDocument;
+  final String programName;
+  final String programId;
+  final bool isQualified;
+
+  TeachingRatingModel({
+    required this.id,
+    required this.teacherName,
+    required this.subjectName,
+    required this.code,
+    required this.group,
+    required this.campusId,
+    required this.teacherId,
+    required this.teacherDocument,
+    required this.programName,
+    required this.programId,
+    required this.isQualified,
+  });
+
+  factory TeachingRatingModel.fromJson(Map<String, dynamic> json) {
+    /*
+
+                                       
+                                        <input type="hidden" name="ase_maa_pea_codigo" value="202402041">
+                                     
+                                        <input type="hidden" name="ase_apd_doc_per_codigo" value="2000004351">
+                                       
+                                    
+    
+     */
+    return TeachingRatingModel(
+      id: json['ase_maa_pea_codigo'],
+      teacherName: json['teacherName'],
+      subjectName: json['apd_asi_nombre'],
+      code: json['ase_apd_asi_codigo'],
+      group: json['ase_apd_agp_grupo'],
+      campusId: json['ase_apd_sed_codigo'],
+      teacherId: json['ase_apd_doc_per_codigo'],
+      teacherDocument: json['ase_apd_doc_codigo'],
+      programName: json['ase_pra_nombre'],
+      programId: json['ase_maa_pra_codigo'],
+      isQualified: json['isQualified'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'teacherName': teacherName,
+      'subjectName': subjectName,
+      'code': code,
+      'group': group,
+      'campusId': campusId,
+      'teacherId': teacherId,
+      'teacherDocument': teacherDocument,
+      'programName': programName,
+      'programId': programId,
+      'isQualified': isQualified,
+    };
+  }
+
+  factory TeachingRatingModel.fromEntity(TeachingRating entity) {
+    return TeachingRatingModel(
+      id: entity.id,
+      teacherName: entity.teacherName,
+      subjectName: entity.subjectName,
+      code: entity.code,
+      group: entity.group,
+      campusId: entity.campusId,
+      teacherId: entity.teacherId,
+      teacherDocument: entity.teacherDocument,
+      programName: entity.programName,
+      programId: entity.programId,
+      isQualified: entity.isQualified,
+    );
+  }
+
+  TeachingRating toEntity() {
+    return TeachingRating(
+      id: id,
+      teacherName: teacherName,
+      subjectName: subjectName,
+      code: code,
+      group: group,
+      campusId: campusId,
+      teacherId: teacherId,
+      teacherDocument: teacherDocument,
+      programName: programName,
+      programId: programId,
+      isQualified: isQualified,
+    );
+  }
+}
