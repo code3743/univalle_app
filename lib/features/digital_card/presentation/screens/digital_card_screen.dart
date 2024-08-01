71b0982d51ec34bb22bb78042e9271196b175505
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-01 09:31:26 -0500

    feat: Update digital card screen layout and styling

--- lib/features/digital_card/presentation/screens/digital_card_screen.dart
+++ lib/features/digital_card/presentation/screens/digital_card_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:univalle_app/config/themes/app_colors.dart';
 import 'package:univalle_app/config/providers/student_use_cases_provider.dart';
 import 'package:univalle_app/features/digital_card/presentation/widgets/student_card.dart';
@@ -11,62 +10,52 @@ class DigitalCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
+
     return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+        ),
         body: SizedBox(
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Column(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              Container(
-                height: size.height * .5,
-                color: AppColors.primaryRed,
+              Column(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity,
+                      color: AppColors.primaryRed,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity,
+                      color: AppColors.white,
+                    ),
+                  )
+                ],
               ),
-              Container(
-                height: size.height * .5,
-                color: AppColors.white,
+              CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: SizedBox(height: size.height * .5 - 60 - (565 * .5)),
+                  ),
+                  Consumer(builder: (_, ref, __) {
+                    return SliverToBoxAdapter(
+                      child: StudentCard(
+                        student: ref.read(studentUseCasesProvider).getStudent(),
+                        width: size.width * .9,
+                      ),
+                    );
+                  })
+                ],
               )
             ],
           ),
-          CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: SizedBox(
-                  height: size.height * .5 - (565 * .5),
-                ),
-              ),
-              Consumer(builder: (_, ref, __) {
-                return SliverToBoxAdapter(
-                  child: StudentCard(
-                    student: ref.watch(studentUseCasesProvider).getStudent(),
-                    width: size.width * .9,
-                  ),
-                );
-              })
-            ],
-          ),
-          Positioned(
-            top: 10,
-            right: 20,
-            child: SafeArea(
-              child: IconButton(
-                  style: IconButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      shadowColor: AppColors.grey,
-                      elevation: 5,
-                      backgroundColor: AppColors.white),
-                  onPressed: () => context.pop(),
-                  icon: const Icon(
-                    Icons.cancel_rounded,
-                    color: AppColors.primaryRed,
-                  )),
-            ),
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
