9e8e1f16b819f8f75f5941f1d8326684ae626688
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-02 14:42:24 -0500

    feat: Update teaching rating functionality

--- lib/features/teaching_rating/domain/repositories/teaching_rating_repository.dart
+++ lib/features/teaching_rating/domain/repositories/teaching_rating_repository.dart
@@ -1,8 +1,11 @@
+import 'package:univalle_app/features/teaching_rating/domain/entities/questions_subject.dart';
+import 'package:univalle_app/features/teaching_rating/domain/entities/review_subject.dart';
 import 'package:univalle_app/features/teaching_rating/domain/entities/teaching_rating.dart';
 
 abstract class TeachingRatingRepository {
   Future<String> authenticateSession(String user, String password);
-  Future<List<TeachingRating>> getTeachingQualifications(String sessionId);
-  Future<void> sendTeachingQualification(
-      TeachingRating teacher, String sessionId);
+  Future<List<TeachingRating>> getTeachingToRatings(String sessionId);
+  Future<void> sendTeachingRating(String sessionId, ReviewSubject review);
+  Future<List<QuestionsSubject>> getQuestionsSubject(
+      String sessionId, TeachingRating teacher);
 }
