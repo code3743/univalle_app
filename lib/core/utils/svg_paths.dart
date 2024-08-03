a21d095b78828501d8c6c6016a88ab3a2203dfa4
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-03 09:29:35 -0500

    feat: Add SVG paths utility class

---
+++ lib/core/utils/svg_paths.dart
@@ -0,0 +1,13 @@
+class SvgPaths {
+  SvgPaths._();
+  static const String _basePath = 'assets/svg/';
+  static const String avarage = '${_basePath}average.svg';
+  static const String credits = '${_basePath}credits.svg';
+  static const String debts = '${_basePath}debts.svg';
+  static const String digitalCard = '${_basePath}digital_card.svg';
+  static const String logo = '${_basePath}logo.svg';
+  static const String ratings = '${_basePath}ratings.svg';
+  static const String restaurant = '${_basePath}restaurant.svg';
+  static const String tabulated = '${_basePath}tabulated.svg';
+  static const String teacherRating = '${_basePath}teacher_rating.svg';
+}
