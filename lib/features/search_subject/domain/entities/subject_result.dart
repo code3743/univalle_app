0e787ad3a4fa5bb3631539c9dae89a88e79c7a29
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-09 11:13:33 -0500

    feat: Update SubjectResult model and add SearchResult model

--- lib/features/search_subject/domain/entities/subject_result.dart
+++ lib/features/search_subject/domain/entities/subject_result.dart
@@ -1,25 +1,35 @@
 class SubjectResult {
-  final String code;
-  final String name;
   final String group;
   final String teacher;
   final String teacherEmail;
   final String program;
   final bool isGeneric;
   final bool isRepeater;
-  final int credits;
-  final String hours;
+  final String schedule;
+  final int capacity;
 
   SubjectResult({
-    required this.code,
-    required this.name,
     required this.group,
     required this.teacher,
     required this.teacherEmail,
     required this.program,
     required this.isGeneric,
     required this.isRepeater,
+    required this.schedule,
+    required this.capacity,
+  });
+}
+
+class SearchResult {
+  final String code;
+  final String name;
+  final int credits;
+  final List<SubjectResult> results;
+
+  SearchResult({
+    required this.code,
+    required this.name,
     required this.credits,
-    required this.hours,
+    required this.results,
   });
 }
