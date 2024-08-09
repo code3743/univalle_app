0a869e5f5ef3b9a38e254946b85915ab559dc796
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-08 21:40:31 -0500

    feat: Rename logout_provider.dart to reset_providers.dart and update dependencies

--- lib/config/providers/reset_providers.dart
+++ lib/config/providers/reset_providers.dart
@@ -1,15 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:univalle_app/config/providers/profile_picture_provider.dart';
+import 'package:univalle_app/config/providers/selected_campus_provider.dart';
 import 'package:univalle_app/config/providers/shared_preferences_provider.dart';
-import 'package:univalle_app/config/providers/student_use_cases_provider.dart';
 import 'package:univalle_app/features/student_grades/presentation/providers/student_grades_provider.dart';
 import 'package:univalle_app/features/student_tabulate/presentation/providers/student_tabulate_provider.dart';
 
-final logoutProvider = FutureProvider.autoDispose<void>((ref) async {
-  final studentUseCase = ref.read(studentUseCasesProvider);
-  await studentUseCase.logout();
+final resetProvider = FutureProvider.autoDispose<void>((ref) async {
   ref.read(profilePictureProvider.notifier).removeImage();
   await ref.read(sharedUtilityProvider).clearStudentData();
+  ref.invalidate(selectedCampusProvider);
   ref.invalidate(studentGradesProvider);
   ref.invalidate(studentTabulateProvider);
   return;
