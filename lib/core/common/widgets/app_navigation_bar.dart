679b5e014c5315449b6bb825a99488535f837b47
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-11 20:49:39 -0500

    feat: Update AppNavigationBar to use Riverpod for state management and implement navigation logic

--- lib/core/common/widgets/app_navigation_bar.dart
+++ lib/core/common/widgets/app_navigation_bar.dart
@@ -1,30 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:univalle_app/config/providers/student_use_cases_provider.dart';
+import 'package:univalle_app/config/routers/app_router.dart';
 import 'package:univalle_app/config/themes/app_colors.dart';
 
-class AppNavigationBar extends StatelessWidget {
+class AppNavigationBar extends ConsumerWidget {
   const AppNavigationBar({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: AppColors.white,
-      selectedItemColor: AppColors.primaryRed,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_rounded),
-          label: 'Inicio',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search_rounded),
-          label: 'Asignaturas',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.notifications_rounded),
-          label: 'Novedades',
-        ),
-      ],
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentIndex = ValueNotifier<int>(0);
+    return ValueListenableBuilder(
+        valueListenable: currentIndex,
+        builder: (context, value, _) {
+          return BottomNavigationBar(
+            backgroundColor: AppColors.white,
+            selectedItemColor: AppColors.primaryRed,
+            onTap: (value) async {
+              final isLogged =
+                  await ref.read(studentUseCasesProvider).isLogged();
+              switch (value) {
+                case 0:
+                  if (!isLogged) return;
+                  ref.read(appRouterProvider).go('/home');
+                  break;
+                case 1:
+                  ref.read(appRouterProvider).go('/subject-search');
+                  break;
+                case 2:
+                default:
+                  break;
+              }
+              currentIndex.value = value;
+            },
+            currentIndex: value,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_rounded),
+                label: 'Inicio',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search_rounded),
+                label: 'Asignaturas',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star_rounded),
+                label: 'Interés',
+              ),
+            ],
+          );
+        });
   }
 }
