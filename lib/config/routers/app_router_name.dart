fe673f5883427c70b09b25a7bf5495e0b4c93a3e
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-11 20:49:22 -0500

    feat: Add AppRouterName class for managing app routes

---
+++ lib/config/routers/app_router_name.dart
@@ -0,0 +1,16 @@
+class AppRouterName {
+  AppRouterName._();
+
+  static const String checkAuth = '/check';
+  static const String home = '/home';
+  static const String subjectSearch = '/subject-search';
+  static const String login = '/login';
+  static const String resetPassword = '/login/reset-password';
+  static const String studentGrades = '/home/student-grades';
+  static const String tabulate = '/home/tabulate';
+  static const String resolution = '/home/resolution';
+  static const String detailSemester = '/home/resolution/detail-semester';
+  static const String digitalCard = '/home/digital-card';
+  static const String teachingRating = '/home/teaching-rating';
+  static const String reviewSubject = '/home/teaching-rating/review-subject';
+}
