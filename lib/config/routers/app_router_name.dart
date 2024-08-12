e17ae397d607fa02c10721192fb526dd50218ffc
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-12 12:41:39 -0500

    feat: Update routing constants to use main screen instead of home screen

--- lib/config/routers/app_router_name.dart
+++ lib/config/routers/app_router_name.dart
@@ -5,11 +5,11 @@ class AppRouterName {
   static const String main = '/main';
   static const String login = '/login';
   static const String resetPassword = '/login/reset-password';
-  static const String studentGrades = '/home/student-grades';
-  static const String tabulate = '/home/tabulate';
-  static const String resolution = '/home/resolution';
-  static const String detailSemester = '/home/resolution/detail-semester';
-  static const String digitalCard = '/home/digital-card';
-  static const String teachingRating = '/home/teaching-rating';
-  static const String reviewSubject = '/home/teaching-rating/review-subject';
+  static const String studentGrades = '/main/student-grades';
+  static const String tabulate = '/main/tabulate';
+  static const String resolution = '/main/resolution';
+  static const String detailSemester = '/main/resolution/detail-semester';
+  static const String digitalCard = '/main/digital-card';
+  static const String teachingRating = '/main/teaching-rating';
+  static const String reviewSubject = '/main/teaching-rating/review-subject';
 }
