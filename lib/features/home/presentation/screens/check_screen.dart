228994dda9967ba3aca1832250d6c92fbe8fc2ce
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-12 12:42:29 -0500

    chore: Update CheckScreen to handle campus change and navigation

--- lib/features/home/presentation/screens/check_screen.dart
+++ lib/features/home/presentation/screens/check_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:univalle_app/config/providers/selected_campus_provider.dart';
 import 'package:univalle_app/config/routers/app_router.dart';
+import 'package:univalle_app/config/routers/app_router_name.dart';
 import 'package:univalle_app/config/themes/app_colors.dart';
 import 'package:univalle_app/config/providers/student_use_cases_provider.dart';
+import 'package:univalle_app/core/common/handlers/handlers.dart';
 import 'package:univalle_app/core/common/widgets/widgets.dart';
 
 class CheckScreen extends ConsumerWidget {
@@ -21,13 +23,21 @@ class CheckScreen extends ConsumerWidget {
           builder: (_, AsyncSnapshot<bool> snapshot) {
             if (snapshot.hasData) {
               Future.microtask(() {
-                ref.read(selectedCampusProvider.notifier).changeCampusById(
-                    snapshot.data!
-                        ? ref.read(studentUseCasesProvider).getStudent().campus
-                        : '00');
+                if (snapshot.data!) {
+                  final campus =
+                      ref.read(studentUseCasesProvider).getStudent().campus;
+                  ref
+                      .read(selectedCampusProvider.notifier)
+                      .changeCampusById(campus);
+                  ref
+                      .read(appRouterProvider)
+                      .go(AppRouterName.main, extra: true);
+                  return;
+                }
                 ref
-                    .read(appRouterProvider)
-                    .go(snapshot.data! ? '/home' : '/login');
+                    .read(selectedCampusProvider.notifier)
+                    .changeCampusById('00');
+                ref.read(appRouterProvider).go(AppRouterName.login);
               });
             }
             if (snapshot.hasError) {
@@ -35,7 +45,10 @@ class CheckScreen extends ConsumerWidget {
                 ref
                     .read(selectedCampusProvider.notifier)
                     .changeCampusById('00');
-                ref.read(appRouterProvider).go('/login');
+                ref
+                    .read(snackBarHandlerProvider)
+                    .showSnackBarError(snapshot.error.toString());
+                ref.read(appRouterProvider).go(AppRouterName.login);
               });
             }
             return const Column(
