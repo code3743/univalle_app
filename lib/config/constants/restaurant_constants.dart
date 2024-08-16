0eed36469b90cc52661be4538de5c073e2cd4124
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-16 17:12:36 -0500

    feat: Add RestaurantConstants for API endpoints

---
+++ lib/config/constants/restaurant_constants.dart
@@ -0,0 +1,8 @@
+class RestaurantConstants {
+  RestaurantConstants._();
+  static const String baseUrl =
+      'https://restauranteuniversitario.univalle.edu.co/restaurante.php';
+  static const String login = '/login';
+  static const String info = '/tiquete_en_linea/comprar';
+  static const String buyLucnhes = '/tiquete_en_linea/pagar';
+}
