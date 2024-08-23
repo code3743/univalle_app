525721bc38a1631c70951a982c8fdb885beee735
code3743 <jota.lopez@correounivalle.edu.co>
2024-08-23 16:12:00 -0500

    feat: Update restaurant screen with home route

--- lib/config/constants/restaurant_constants.dart
+++ lib/config/constants/restaurant_constants.dart
@@ -3,6 +3,7 @@ class RestaurantConstants {
   static const String baseUrl =
       'https://restauranteuniversitario.univalle.edu.co/restaurante.php';
   static const String login = '/login';
+  static const String home = '/panel/seleccionarGrupoTrabajo';
   static const String info = '/tiquete_en_linea/comprar';
   static const String buyLucnhes = '/tiquete_en_linea/pagar';
 }
