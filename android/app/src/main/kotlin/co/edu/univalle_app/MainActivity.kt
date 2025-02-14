2e1f100e0c665010e2040c32da73f69673065992
code3743 <jota.lopez@correounivalle.edu.co>
2025-02-14 12:04:14 -0500

    Update project configuration and assets; add iOS bridging header, new app icons, and adjust Gradle properties

--- android/app/src/main/kotlin/co/edu/univalle_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package co.edu.univalle_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/univalle_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.univalle_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
