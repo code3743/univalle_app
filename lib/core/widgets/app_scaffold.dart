9eefdd38324df34930257c42dc3060ef08fe4bb2
code3743 <jota.lopez@correounivalle.edu.co>
2026-09-17 11:51:58 -0500

    feat: add refresh functionality to NewsView and improve AppScaffold styling

--- lib/core/widgets/app_scaffold.dart
+++ lib/core/widgets/app_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:univalle_app/core/theme/app_colors.dart';
 
 import '../theme/app_spacing.dart';
 
@@ -31,6 +32,7 @@ class AppScaffold extends StatelessWidget {
                   style: Theme.of(context).textTheme.titleLarge,
                 ),
                 actions: actions,
+                surfaceTintColor: AppColors.white,
               )
             : null);
     final content = Padding(padding: padding, child: body);
